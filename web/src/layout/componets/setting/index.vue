e14c0262db151e12a18a8a10e60b29a13c058f5e
xianglong <56494565@qq.com>
2024-08-30 18:01:57 +0800

    no message

--- web/src/layout/componets/setting/index.vue
+++ web/src/layout/componets/setting/index.vue
@@ -11,41 +11,72 @@ import {useLayout} from "@/stores/layout.js";
     </a-divider>
 
     <a-space direction="vertical" :size="16" style="display: block;">
-      <a-row :gutter="24">
+      <a-row :gutter="24" justify="space-between">
         <a-col :span="12">
-          <a-tooltip content="纵向">
-            <div class="layout-list shadow">
-              <a-row :gutter="5" justify="center" align="center" style="padding-top: 8px">
-                <a-col flex="17px">
-                  <div class="layout-menu"></div>
-                </a-col>
-                <a-col flex="64px" class="layout-container">
-                  <div class="layout-head"></div>
-                  <div class="layout-body"></div>
-                </a-col>
-              </a-row>
+          <div class="relative layout-list shadow check">
+            <a-tooltip content="纵向">
+              <a-space direction="vertical" :size="16">
+                <a-row :gutter="2" justify="space-between">
+                  <a-col flex="18px">
+                    <div class="layout-menu"></div>
+                  </a-col>
+                  <a-col flex="auto">
+                    <div class="layout-head"></div>
+                    <div class="layout-body" style="margin-top: 3px"></div>
+                  </a-col>
+                </a-row>
+              </a-space>
               <icon-check-circle-fill v-if="useLayout().layout==='vertical'" size="20" class="icon-circle"/>
-            </div>
-          </a-tooltip>
+            </a-tooltip>
+          </div>
         </a-col>
 
         <a-col :span="12">
-          <a-tooltip content="经典">
-            <div class="layout-list shadow">
-              <a-row :gutter="5" justify="center" align="center" style="padding-top: 8px">
-                <a-col flex="17px">
-                  <div class="layout-head"></div>
-                </a-col>
-                <a-col flex="17px">
-                  <div class="layout-menu"></div>
-                </a-col>
-                <a-col flex="64px">
-                  <div class="layout-body"></div>
-                </a-col>
-              </a-row>
+          <div class="relative layout-list shadow">
+            <a-tooltip content="经典">
+              <a-space direction="vertical" :size="16">
+                <a-row :gutter="2" justify="space-between">
+                  <a-col>
+                    <div class="layout-head" style="height: 13px"></div>
+                  </a-col>
+                  <a-col style="margin-top: 3px">
+                    <a-row :gutter="2" justify="center">
+                      <a-col flex="20px">
+                        <div class="layout-menu" style="height: 45px"></div>
+                      </a-col>
+                      <a-col flex="auto">
+                        <div class="layout-body" style="height: 45px"></div>
+                      </a-col>
+                    </a-row>
+                  </a-col>
+                </a-row>
+              </a-space>
               <icon-check-circle-fill v-if="useLayout().layout==='classic'" size="20" class="icon-circle"/>
-            </div>
-          </a-tooltip>
+            </a-tooltip>
+          </div>
+        </a-col>
+
+        <a-col :span="12">
+          <div class="relative layout-list shadow">
+            <a-tooltip content="横向">
+              <a-space direction="vertical" :size="16">
+                <a-row :gutter="2" justify="space-between">
+                  <a-col>
+                    <div class="layout-head" style="height: 13px"></div>
+                  </a-col>
+                  <a-col style="margin-top: 3px">
+                    <a-row :gutter="2" justify="center">
+                     
+                      <a-col flex="auto">
+                        <div class="layout-body" style="height: 45px"></div>
+                      </a-col>
+                    </a-row>
+                  </a-col>
+                </a-row>
+              </a-space>
+              <icon-check-circle-fill v-if="useLayout().layout==='classic'" size="20" class="icon-circle"/>
+            </a-tooltip>
+          </div>
         </a-col>
       </a-row>
     </a-space>
@@ -55,22 +86,23 @@ import {useLayout} from "@/stores/layout.js";
 
 <style scoped>
 .layout-list {
-  width: 100px;
-  height: 70px;
+  width: 93px;
+  height: 63px;
   cursor: pointer;
-  border-radius: 2px;
+  padding: 7px;
+  border-radius: 8px;
   position: relative;
 }
 
 .layout-menu {
-  height: 58px;
+  height: 63px;
   background-color: rgb(var(--primary-6));
   width: 100%;
   border-radius: 4px;
 }
 
 .layout-head {
-  height: 20%;
+  height: 15px;
   width: 100%;
   background-color: rgb(var(--primary-4));
   border-radius: 4px;
@@ -78,11 +110,10 @@ import {useLayout} from "@/stores/layout.js";
 }
 
 .layout-body {
-  height: 67%;
+  height: 43px;
   width: 100%;
   background-color: rgb(var(--primary-2));
   border-radius: 4px;
-  margin-top: 3px;
   border: 1px dashed rgb(var(--primary-1));
 }
 
@@ -90,6 +121,10 @@ import {useLayout} from "@/stores/layout.js";
   position: absolute;
   color: rgb(var(--primary-6));
   right: 15px;
-  bottom: 10px;
+  bottom: 15px;
+}
+
+.check {
+  border: 1px dashed rgb(var(--primary-6));
 }
 </style>
